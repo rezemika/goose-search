32e6ac307a207c5a66fbdd646173dde8e3934bfb
Michael Marx <michael-marx@protonmail.ch>
2017-08-04 13:11:19 +0200

    Améliore les messages de debug en console

--- assets/js/search_filters.js
+++ assets/js/search_filters.js
@@ -2,9 +2,9 @@
 
 function filter_results_handler (object) {
     var input, filter, status, ul, li, tags, i, spans, filter, tags_field, tags_array;
-    console.log('Filter ' + object.value + ' used.');
     filter = object.value;
     status = object.checked;
+    console.log('Filter "' + object.value + '" used (now ' + status + ').');
     ul = document.getElementById("results_list");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
@@ -12,9 +12,9 @@ function filter_results_handler (object) {
         var last_span = spans[spans.length - 1];
         tags_field = last_span.innerHTML;
         tags_array = tags_field.split(';');
-        console.log(filter + ' ' + status);
-        console.log(tags_array);
-        console.log(filter in tags_array);
+        console.log('Result ' + i + '. Tags: ' + tags_array);
+        // Is the tag present in this result?
+        console.log(tags_array.indexOf(filter) != -1);
         console.log('');
         if (tags_array.indexOf(filter) != -1) {
             if (status == true) {
