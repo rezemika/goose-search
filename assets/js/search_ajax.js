d397a1162feec0488fe3ef69da4648a8af3741c0
Michael Marx <michael-marx@protonmail.ch>
2017-08-09 15:06:48 +0200

    Affiche un message en cas d'erreur 500

--- assets/js/search_ajax.js
+++ assets/js/search_ajax.js
@@ -4,7 +4,7 @@
 function request_results (radius, userLatitude, userLongitude, searchPresetId, no_private, was_limited) {
     
     if (was_limited == true) {
-        $('#geo_results').html("<center><em>Trop peu de requêtes ont été faites en trop peu de temps. Merci d'attendre quelques secondes avant de raffraichir la page.</em></center>")
+        $('#geo_results').html("<center><em>Trop peu de requêtes ont été faites en trop peu de temps. Merci d'attendre quelques secondes avant de raffraichir la page.</em></center>");
         $('#geo_results').attr("aria-live", "assertive");
         $('#geo_results').attr("aria-busy", "false");
         console.log("Request blocked : too much requests in too little time.");
@@ -26,34 +26,34 @@ function request_results (radius, userLatitude, userLongitude, searchPresetId, n
         // Handles a successful response.
         success: function (json) {
             // Logs the returned json to the console.
-            console.log('received:', JSON.stringify(json, null, 2))
+            console.log('received:', JSON.stringify(json, null, 2));
             if (json.status != "ok") {
                 console.log('Error : Request status != "ok"');
-                console.log(json.debug_msg)
-                $('#geo_results').html("<center><em>" + json.err_msg + "</em></center>")
+                console.log(json.debug_msg);
+                $('#geo_results').html("<center><em>" + json.err_msg + "</em></center>");
                 $('#geo_results').attr("aria-live", "assertive");
                 $('#geo_results').attr("aria-busy", "false");
                 return
             }
             if (!json.content.length) {
                 console.log('json content is empty')
-                $('#geo_results').html("<center><em>Pas de résultats.</em></center>")
+                $('#geo_results').html("<center><em>Pas de résultats.</em></center>");
                 $('#geo_results').attr("aria-live", "assertive");
                 $('#geo_results').attr("aria-busy", "false");
-                console.log('Success, but no results !')
+                console.log('Success, but no results !');
                 return
             }
             // Allowings linebreaks.
-            const htmlOutput = json.content.map(part => '\n\n' + part.replace(/\r?\n/g, '<br>\n')).join('')
+            const htmlOutput = json.content.map(part => '\n\n' + part.replace(/\r?\n/g, '<br>\n')).join('');
             // Fills the page.
-            $('#geo_results').html('<ul id="results_list">' + htmlOutput + '</ul>')
+            $('#geo_results').html('<ul id="results_list">' + htmlOutput + '</ul>');
             // Updates the ARIA.
             $('#geo_results').attr("aria-live", "assertive");
             $('#geo_results').attr("aria-busy", "false");
             // Makes popovers open on top on small screens.
             if ($(document).width() < 768) {
                 console.log("Small screen detected, popovers will open on top.");
-                var popovers = document.querySelectorAll('[data-toggle="popover"]')
+                var popovers = document.querySelectorAll('[data-toggle="popover"]');
                 for (i=0; i < popovers.length; i++) {
                         popovers[i].setAttribute("data-placement", "top");
                 }
@@ -63,14 +63,14 @@ function request_results (radius, userLatitude, userLongitude, searchPresetId, n
             // Loads filters.
             $('#results_filters').html(json.filters);
             console.log(json.filters);
-            console.log('Success !')
+            console.log('Success !');
         },
 
         // Handles a non-successful response.
         error: function (xhr, errmsg, err) {
-            if (errmsg || err) $('#geo_results').html({errmsg, err})
+            $('#geo_results').html("<center><em>Erreur 500.</em></center><br/><center><em>Désolé, une erreur non prise en charge s'est produite.</em></center>")
             // Provides a bit more info about the error to the console.
-            console.log(JSON.stringify(xhr, null, 2))
+            console.log(JSON.stringify(xhr, null, 2));
         },
     })
 };
