3c4cb447a4460d60245b96d2233272e90e83d891
Michael Marx <michael-marx@protonmail.ch>
2017-08-10 20:00:38 +0200

    Corrige le filtrage des tags

--- assets/js/search_filters.js
+++ assets/js/search_filters.js
@@ -1,29 +1,40 @@
 // Allows to filter the results a posteriori with checkboxes.
 
 function filter_results_handler (object) {
-    var input, filter, status, ul, li, tags, i, spans, filter, tags_field, tags_array;
-    filter = object.value;
-    status = object.checked;
-    console.log('Filter "' + object.value + '" used (now ' + status + ').');
+    var ul, li, filters, spans, last_span, result_tags, active_filters, show_result;
+    var i, j;
     ul = document.getElementById("results_list");
     li = ul.getElementsByTagName("li");
+    filters = document.getElementsByName("result_filter");
+    active_filters = []
+    // Stores the values of the active filters (those ticked).
+    for (i = 0; i < filters.length; i++) {
+        if (filters[i].checked) {
+            active_filters.push(filters[i].value);
+        }
+    }
+    console.log("Active filters: " + active_filters);
     for (i = 0; i < li.length; i++) {
         spans = li[i].getElementsByTagName("span");
-        var last_span = spans[spans.length - 1];
-        tags_field = last_span.innerHTML;
-        tags_array = tags_field.split(';');
-        console.log('Result ' + i + '. Tags: ' + tags_array);
-        // Is the tag present in this result?
-        console.log(tags_array.indexOf(filter) != -1);
-        console.log('');
-        if (tags_array.indexOf(filter) != -1) {
-            if (status == true) {
-                li[i].style.display = "block";
-            }
-            else {
-                li[i].style.display = "none";
+        last_span = spans[spans.length - 1];
+        result_tags = last_span.innerHTML.split(';');
+        // Checks all the result tags are in the active filters.
+        show_result = true;
+        for (j = 0; j < result_tags.length; j++) {
+            if (active_filters.indexOf(result_tags[j]) == -1) {
+                show_result = false;
             }
         }
+        if (show_result == true) {
+            console.log("Result " + i + ": showed");
+            li[i].style.display = "block";
+        }
+        else {
+            console.log("Result " + i + ": hidden");
+            li[i].style.display = "none";
+        }
+        console.log("Tags: " + result_tags);
+        console.log('');
     }
     return
 }
