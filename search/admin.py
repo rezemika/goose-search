d4029410f55be84a25d3e648bae257f5e5bd879f
Michael Marx <michael-marx@protonmail.ch>
2017-09-28 23:33:02 +0200

    Ajoute l'ID des presets dans la page d'admin

--- search/admin.py
+++ search/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from search.models import SearchPreset
 
 class SearchPresetAdmin(admin.ModelAdmin):
-    list_display   = ("name", )
+    list_display   = ("name", "id")
 
 admin.site.register(SearchPreset, SearchPresetAdmin)
